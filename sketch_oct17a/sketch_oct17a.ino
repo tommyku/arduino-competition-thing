b7c700ed193033ee7bdc989d4100054c870ad71a
Tommy Ku <zoo1929@outlook.com>
2017-10-28 17:31:33 +0800

    ADD: locking part working

--- sketch_oct17a/sketch_oct17a.ino
+++ sketch_oct17a/sketch_oct17a.ino
@@ -14,13 +14,17 @@ const int SERVO_CLOSE = 90;
 // pins
 const int SERVO_PIN = 8;
 const int SPEAKER_PIN = 9;
+const int LED_PIN_RED = 5;
+const int LED_PIN_GREEN = 6;
+const int DISTANCE_SENSOR_PIN = A0;
 
 // state variables
 const int APP_STATE = 0;
-const int SERVO = 1;
+const int INIT_DISTANCE = 1;
+const int BOX_OPENED = 2;
 
 // run time states
-int states[] = {INITIAL_STAGE};
+int states[] = {INITIAL_STAGE, 0, true};
 int readings[] = {};
 
 Servo servo;
@@ -29,64 +33,93 @@ void servoAction() {
   if (states[APP_STATE] == INITIAL_STAGE || states[APP_STATE] == ARRIVAL_STAGE) {
     servo.write(SERVO_OPEN);
   }
-  if (states[APP_STATE] == CLOSING_STAGE || states[APP_STATE] == CARRYING_STAGE) {
+  if (states[APP_STATE] == CLOSING_STAGE || states[APP_STATE] == CARRYING_STAGE || states[APP_STATE] == ERROR_STAGE) {
     servo.write(SERVO_CLOSE);
   }
 }
 
+bool isOpen() {
+  int val = analogRead(DISTANCE_SENSOR_PIN);
+  return (val - states[INIT_DISTANCE] < 100);
+}
+
 void errorAction() {
-  if (states[APP_STAGE] != ERROR_STATE) {
+  if (states[APP_STATE] != ERROR_STAGE) {
     return;
   }
 
-  tone(SPEAKER_PIN, 1000);
-  // turn on LED here
+  //tone(SPEAKER_PIN, 1000);
+
+  digitalWrite(LED_PIN_GREEN, LOW);
+  digitalWrite(LED_PIN_RED, HIGH);
 }
 
-void calculateNewState() {
+int calculateNewState() {
   switch (states[APP_STATE]) {
     case INITIAL_STAGE:
-      return CLOSING_STAGE;
+      if (!states[BOX_OPENED]) { // closed
+        delay(4000); // give it enough time to rest
+        return CLOSING_STAGE;
+      } else {
+        return INITIAL_STAGE;
+      }
       break;
     case CLOSING_STAGE:
-      // useless?
       return CARRYING_STAGE;
       break;
     case CARRYING_STAGE:
-      if (abs(state[DISTANCE_SENSOR] - reading[DISTANCE_SENSOR]) > 10 || accelerometer.shaking || reading[OPENED]) {
+      /*
+      if (states[BOX_OPENED] || abs(states[DISTANCE_SENSOR] - reading[DISTANCE_SENSOR]) > 10 || accelerometer.shaking) {
         // error
-        return ERROR_STATE;
+        return ERROR_STAGE;
       } else if (reading[GPS] == states[DESTINATION_GPS]) {
         return ARRIVAL_STAGE;
       }
+      */
       break;
     case ARRIVAL_STAGE:
-      if ((reading[OPENED] && reading[GPS] != states[DESTINATION_GPS]) || accelerometer.shaking) {
-        return ERROR_STATE;
+      /*
+      if ((states[BOX_OPENED] && readings[GPS] != states[DESTINATION_GPS]) || accelerometer.shaking) {
+        return ERROR_STAGE;
       } else if (reading[GPS] != states[DESTINATION_GPS]) {
         return CARRYING_STAGE;
       }
+      */
       break;
-    case ERROR_STATE:
-      return ERROR_STATE;
+    case ERROR_STAGE:
+      return ERROR_STAGE; // always stays in error state
     default:
       return states[APP_STATE];
   }
 }
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("hello world!");
+
   // initialize servo
   servo.attach(SERVO_PIN);
+  servo.write(SERVO_OPEN);
 
   // initialize spaker
   pinMode(SPEAKER_PIN, OUTPUT);
+  noTone(SPEAKER_PIN);
+
+  // initialize distance sensor (must be open at this point)
+  states[INIT_DISTANCE] = analogRead(DISTANCE_SENSOR_PIN);
+
+  pinMode(LED_PIN_RED, OUTPUT);
+  pinMode(LED_PIN_GREEN, OUTPUT);
+  digitalWrite(LED_PIN_RED, LOW);
+  digitalWrite(LED_PIN_GREEN, HIGH);
 }
 
 void loop() {
   // detect new state
-  readings = {....};
+  /*readings = {....};*/
 
   // changes states (based on readings)
+  states[BOX_OPENED] = isOpen();
   states[APP_STATE] = calculateNewState();
 
   // call the hardwares after state change
