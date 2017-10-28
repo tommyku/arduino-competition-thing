37b3790a03fa66022609cf1a9f1a8a8136c7285b
Tommy Ku <zoo1929@outlook.com>
2017-10-28 19:06:04 +0800

    ADD: mostly logic

--- sketch_oct17a.gps.ino
+++ sketch_oct17a.gps.ino
@@ -1,6 +1,12 @@
 #define GPSECHO  true
 
 #include <Servo.h>
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
+// GPS
+SoftwareSerial mySerial(3, 2);
+Adafruit_GPS GPS(&mySerial);
 
 // application states
 const int INITIAL_STAGE = 0;
@@ -35,12 +41,17 @@ const int READ_LON = 2;
 // Set destination
 float desLat = 22.337255;
 float desLong = 114.263155;
+bool fakeDestination = true;
 
 // run time states
 int states[] = {INITIAL_STAGE, 0, true, false, false};
 int readings[] = {0, 0, 0};
 
 Servo servo;
+
+boolean usingInterrupt = false;
+
+void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
 
 void servoAction(bool stateChanged) {
   if (!stateChanged) {
@@ -63,14 +74,12 @@ bool isOpen() {
 }
 
 bool isAtDestination() {
-  return false;
   float sq_distance = (readings[READ_LAT]- desLat) * (readings[READ_LAT]- desLat)
     + (readings[READ_LON] - desLong) * (readings[READ_LON] - desLong);
   return sq_distance < 10000;
 }
 
 bool isShaked() {
-  return false;
 }
 
 void errorAction() {
@@ -98,7 +107,7 @@ int calculateNewState() {
       return CARRYING_STAGE;
       break;
     case CARRYING_STAGE:
-      if (states[BOX_OPENED] || states[SHAKED]) {
+      if (states[BOX_OPENED]) {
         return ERROR_STAGE;
       }
       if (states[ARRIVED]) {
@@ -135,6 +144,13 @@ void setup() {
   Serial.begin(115200);
   Serial.println("hello world!");
 
+  // initialize GPS
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  GPS.sendCommand(PGCMD_ANTENNA);
+  useInterrupt(true);
+
   // initialize servo
   servo.attach(SERVO_PIN);
   servo.write(SERVO_OPEN);
@@ -153,14 +169,70 @@ void setup() {
   digitalWrite(LED_PIN_GREEN, HIGH);
 }
 
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+#endif
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
 
 void loop() {
   // read the values from sensors
+  // if a sentence is received, we can check the checksum, parse it...
+  /*
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences! 
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+  */
+
+/*
+  Serial.print("\nTime: ");
+  Serial.print(GPS.hour, DEC); Serial.print(':');
+  Serial.print(GPS.minute, DEC); Serial.print(':');
+  Serial.print(GPS.seconds, DEC); Serial.print('.');
+  Serial.println(GPS.milliseconds);
+  Serial.print("Date: ");
+  Serial.print(GPS.day, DEC); Serial.print('/');
+  Serial.print(GPS.month, DEC); Serial.print("/20");
+  Serial.println(GPS.year, DEC);
+  Serial.print("Fix: "); Serial.print((int)GPS.fix);
+  Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+*/
+
+/*
+  if (GPS.fix) {
+    readings[READ_LAT] = GPS.latitudeDegrees;
+    readings[READ_LON] = GPS.longitudeDegrees;
+  }
+  */
 
   // changes states (based on readings)
   states[BOX_OPENED] = isOpen();
   states[ARRIVED] = isAtDestination();
-  states[SHAKED] = isShaked();
   int originalState = states[APP_STATE];
   states[APP_STATE] = calculateNewState();
   bool stateChanged = originalState != states[APP_STATE];
